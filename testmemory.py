0fecfb81f1505b2dcff42ff8d4afaffbcbadd347
itsskiweee <96640603+itsskiwee@users.noreply.github.com>
2025-12-23 21:15:11 -0500

    Add test for memory functions in testmemory.py

---
+++ testmemory.py
@@ -0,0 +1,7 @@
+from memory.memory import add_memory, get_recent_memories
+
+add_memory("test", "This is a test memory")
+memory = get_recent_memory()
+print("Recent memory:")
+from m in memories:
+  print(m)
